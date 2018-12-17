9313520941c45fc515c988538c8d8dfdf8d8038c
Uli Hitzel <uli.hitzel@gmail.com>
2018-12-17 10:07:43 +0000

    new Linux binary

--- releases/gen_Linux-binary.sh
+++ releases/gen_Linux-binary.sh
@@ -2,5 +2,5 @@ rm -rf dist build
 cp ../b9y_cli/__main__.py b9y-cli.py
 pyinstaller --onefile b9y-cli.py
 mv dist/b9y-cli .
-rm b9y-cli-linux-386.zip
-zip b9y-cli-linux-386.zip b9y-cli
+rm b9y-cli-linux-amd64.zip
+zip b9y-cli-linux-amd64.zip b9y-cli
